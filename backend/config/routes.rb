7724de98233a518ca1d8d288c02b8927ec91034c
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-02 22:11:01 +0000

    update

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+
+  get "home/index"
+
+
   resources :orders
   resources :products
   
 
   post 'create-checkout-session', to: 'checkout#create_checkout_session', as: 'create_checkout_session'
   post 'webhook', to: 'checkout#stripe_webhook', as: 'stripe_webhook'
+
+
+  get '*path', to: 'home#index', constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 
   
 end
