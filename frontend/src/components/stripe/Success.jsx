f91984cd2c6b1b02eb126c990a0e11535e24aecc
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-03 21:52:40 +0000

    update

--- frontend/src/components/stripe/Success.jsx
+++ frontend/src/components/stripe/Success.jsx
@@ -1,9 +1,35 @@
+import Footer from "../front_page/Footer"
+import Navbar from "../front_page/Navbar"
+
 const Success = () => {
 
     return (
 
         <>
-        Success
+        <Navbar/>
+        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">Payment Successful</p>
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+            Your order is confirmed.
+          </h1>
+          <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+            You will recieve an email confirmation shortly.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Continue Shopping
+            </a>
+            <a href="#" className="text-sm font-semibold text-gray-900">
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </main>
+        <Footer />
         
         </>
     )
