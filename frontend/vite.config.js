e27f078666f1779919bf4768b9d123c1347c6db9
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-03 00:41:39 +0000

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'; // or '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 import svgr from "vite-plugin-svgr";
-
-
-
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    svgr()
+    svgr(),
   ],
+  base: '/', // ✅ Ensure correct path for production
   server: {
-    historyApiFallback: true,
+    middlewareMode: true, // ✅ Allows backend to handle routing
   },
   resolve: {
     alias: {
