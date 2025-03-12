bce2a5efb708187597ff738c6e4b98d2fd886ece
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-12 17:04:30 +0000

    update

--- backend/app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-# app/helpers/application_helper.rb
-def vite_asset_path(filename)
-    manifest_path = Rails.root.join('public', 'vite', 'manifest.json')
-    if File.exist?(manifest_path)
-      manifest = JSON.parse(File.read(manifest_path))
-      manifest[filename]["file"] if manifest[filename]
-    end
-  end
-  
