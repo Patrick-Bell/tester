44d96f4ea5d8d1b33555ae8102ecac8005b83067
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-17 14:00:50 +0000

    updates

--- backend/config/initializers/assets.rb
+++
@@ -1,9 +0,0 @@
-# config/initializers/assets.rb
-
-# Add Vite output directory to the asset load paths
-Rails.application.config.assets.paths << Rails.root.join('public', 'vite', 'assets')
-
-# Optionally add the manifest file for Rails to reference in production
-Rails.application.config.assets.configure do |env|
-  env.append_path Rails.root.join('public', 'vite', 'assets')
-end
