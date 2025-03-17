fe1fec08ee2e86ddb35130dbd487dbc8b21a5063
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-17 23:40:53 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,6 @@ Rails.application.routes.draw do
   end
 
   # Catch-all for React Frontend Routing
- get '*path', to: 'home#index', constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '*path', to: 'home#index', constraints: ->(req) { !req.path.start_with?('/api') }
 
 end
