3b65e6a164efa5e6a3e1322aff64fb9e2c699d53
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-17 00:32:27 +0000

    update

---
+++ backend/config/initializers/vite.rb
@@ -0,0 +1,5 @@
+ViteRuby.configure do |config|
+    config.vite_assets = 'vite_assets' # optional folder for the build
+    config.vite_ruby = 'vite_ruby'     # optional ruby helper for vite
+  end
+  
