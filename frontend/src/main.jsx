e6af2894b18533de7ab71256f7c434a2baed6840
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-20 13:00:23 +0000

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { CartProvider } from './components/context/CartContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
