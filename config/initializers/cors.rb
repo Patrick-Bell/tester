bb09f0e6d896537e5e5d4983f93d37c9ccfc2cea
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-20 14:06:08 +0000

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://minifigmania-47fa062622db.herokuapp.com/'
+    origins 'https://minifigmania-47fa062622db.herokuapp.com'
 
     resource "*",
       headers: :any,
@@ -9,11 +9,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'http://localhost:5173/'
+    origins 'http://localhost:5173'
 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   # Root Route
   root 'home#index'
   get '/', to: 'home#index'
-  get '/admin', to: 'home#index'
 
   # API Routes
-  scope :api do
+  scope '/api' do
     # Devise authentication routes (No need to define manually)
     devise_for :users, path: '', path_names: {
       sign_in: 'login',
@@ -35,7 +34,7 @@ Rails.application.routes.draw do
 
   # Catch-all for React Frontend Routing
 
-  get '*path', to: 'home#index'  # This sends all other routes to index.html
+  get '*path', to: 'home#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 
 
   
