7da01ccb7df5c34b62a9605b2ee82cb2686691aa
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-25 20:09:25 +0000

    update

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ Bundler.require(*Rails.groups)
 module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 7.0
 
     config.active_record.queues = :primary
 
