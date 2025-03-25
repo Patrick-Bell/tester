d6027b86e399d5dd61f9ab40f5c6c834d9314b4c
Patrick Bell <patrick@Patricks-Laptop.local>
2025-03-25 01:16:50 +0000

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
-    Rails.logger.debug "Rendering the index page..."
-    Rails.logger.debug "Request params: #{params.inspect}"
-    render file: Rails.root.join('public/index.html'), layout: false
-    Rails.logger.debug "Index page rendered successfully!"
+    render file: 'public/index.html', layout: false
   end
 end
